3ee197550576ddeb6dd67180630c9e4152f1f5f2
n-ishfaq <157939748+n-ishfaq@users.noreply.github.com>
2024-03-23 09:17:11 -0700

    Create Deep_thought.py

---
+++ Problemset1/Deep_thought.py
@@ -0,0 +1,5 @@
+deep=input("What is the answer to Great Questions of the Life, The Universe, and Everything? ").lower()
+if(deep=="42" or deep=="forty-two"or deep=="forty two"):
+    print("Yes")
+else:
+    print("No")    
