5c3283d0b77740823e589613f6e626e2d3698b00
n-ishfaq <157939748+n-ishfaq@users.noreply.github.com>
2024-03-23 09:19:25 -0700

    Create Home_Federal_Savings.py

---
+++ Problemset1/Home_Federal_Savings.py
@@ -0,0 +1,11 @@
+def greeting(a):
+    if a=="hello":
+      return 0
+    elif a=="h":
+      return 20
+    else:
+      return 100          
+
+us=input("Greeting").strip()
+print(greeting(us))
+
